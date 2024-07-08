504da688940b8f2970ac3fecee10647782772dbe
Shubham <gulhane383@gmail.com>
2024-07-08 23:24:43 +0530

    making mivie container in working

--- src/components/Browse.js
+++ src/components/Browse.js
@@ -1,21 +1,17 @@
-import React, { useEffect } from 'react'
-import { API_OPTIONS } from '../utils/constants';
+
 import Header from './Header'
+import useNowPlayingMovies from '../hooks/useNowPlyingMovies'
+import MainContainer from './MainContainer';
+import SecondaryContainer from './SecondaryContainer';
 
 const Browse = () => {
-
-  const getNowPlayingMovies = async () =>{
-    const data = await fetch('https://api.themoviedb.org/3/movie/now_playing?page=1', API_OPTIONS);
-     const json =await data.json();
-     console.log(json.results)
-  };
-  useEffect(() => {
-    getNowPlayingMovies();
-  },[])
+    useNowPlayingMovies();
   return (
 
     <div>
       <Header />
+      <MainContainer></MainContainer>
+      <SecondaryContainer></SecondaryContainer>
     </div>
   )
 }
--- src/utils/appStore.js
+++ src/utils/appStore.js
@@ -1,10 +1,12 @@
 import { configureStore } from '@reduxjs/toolkit'
+import moviesSlice from './moviesSlice';
 import userReducer from "./userSlice"
 
 const appStore = configureStore({
   reducer: {
     user: userReducer,
+    movies: moviesSlice,
   },
-})
+});
 
 export default appStore;
