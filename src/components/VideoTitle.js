8271b711d9f79c228445e096637447409474c0b2
Shubham <gulhane383@gmail.com>
2024-07-09 22:49:24 +0530

    show video trailer  using MovieSlice (Slice)

--- src/components/VideoTitle.js
+++ src/components/VideoTitle.js
@@ -1,8 +1,16 @@
 import React from 'react'
 
-const VideoTitle = () => {
+const VideoTitle = ({title, overview}) => {
   return (
-    <div>VideoTitle</div>
+    <div className='mt-36 px-12'>
+      <h1 className='font-bold text-6xl'>{title}</h1>
+      <p className='w-3/6 text-lg  mt-5'>{overview}</p>
+
+      <div className='mt-3'> 
+        <button className='  bg-gray-500 p-4 px-10 opacity-50 hover:opacity-100  text-white rounded-md'> ▶Play</button>
+        <button className='bg-gray-500 p-4 px-10 opacity-50 mx-5 hover:opacity-100  text-white rounded-md'>More</button>
+      </div>
+    </div>
   )
 }
 
--- src/utils/moviesSlice.js
+++ src/utils/moviesSlice.js
@@ -4,15 +4,19 @@ const moviesSlice = createSlice({
   name: 'movies',
   initialState: {
    nowPlayingMovies: null,
+   trailerVideo: null,
   },
   reducers: {
     addNowPlayingMovies : (state, action) => {
         state.nowPlayingMovies = action.payload
     },
+    addTrailerVideo : (state, action) => {
+      state.trailerVideo = action.payload
   },
-})
+},
+});
 
 // Action creators are generated for each case reducer function
-export const { addNowPlayingMovies} = moviesSlice.actions
+export const { addNowPlayingMovies, addTrailerVideo } = moviesSlice.actions
 
 export default moviesSlice.reducer
